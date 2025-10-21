cd9b98f3a7134371363305b860953f56c9dd836b
177159 <henick408@gmail.com>
2025-10-21 10:01:34 +0000

    COM2_1.3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 (function() {
   const example = document.getElementById('example')
   const cw1 = document.getElementById('cw1')
+  const cw1Input = document.getElementById("cw1Input")
   const cw2 = document.getElementById('cw2')
   const cw3 = document.getElementById('cw3')
   const answer = document.getElementById('answer')
@@ -17,6 +18,23 @@
   cw1.addEventListener("click", function() {
 
     answer.innerHTML = "<p>Loading...</p>"
+
+    if (cw1Input.value.length != 0) {
+      fetch("https://jsonplaceholder.typicode.com/posts/" + cw1Input.value)
+        .then(response => response.json())
+        .then(post => {
+          console.log("Pobrano post:", post);
+
+          let html = `
+             <div class="post">
+               <h3>${post.id}. ${post.title}</h3>
+               <p>${post.body}</p>
+             </div>
+          `;
+          answer.innerHTML = html;
+        });
+      return;
+    };
 
     fetch("https://jsonplaceholder.typicode.com/posts")
       .then(response => response.json())
